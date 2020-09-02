f509894bf18ed92452b7769bce9a0b0c2861284f
wild1k <cssndrchmbrln@gmail.com>
2020-09-01 22:41:33 -0700

    Updates

---
+++ precache-manifest.80a324ca5ec9a7c1939c3ad985f2ab9b.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d74ea0adcd68701a2c3157ec86d5fba7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e171118343558c60bd35",
+    "url": "/static/css/main.75a4ba1a.chunk.css"
+  },
+  {
+    "revision": "23516e1795693ae7dda5",
+    "url": "/static/js/2.b86efad0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.b86efad0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e171118343558c60bd35",
+    "url": "/static/js/main.c1d5d75d.chunk.js"
+  },
+  {
+    "revision": "2b60c71335a47ae8c7d0",
+    "url": "/static/js/runtime-main.180e305e.js"
+  },
+  {
+    "revision": "69cdbb68da8f4e0880ed2ee55c002dff",
+    "url": "/static/media/burgerApp.69cdbb68.png"
+  },
+  {
+    "revision": "308c3d8c5df5b1d72096712b14e87cf5",
+    "url": "/static/media/gatherNow.308c3d8c.png"
+  },
+  {
+    "revision": "6019d76a8344b7d2852a27c7166fb220",
+    "url": "/static/media/passwordGen.6019d76a.png"
+  },
+  {
+    "revision": "7be1ac204a186f159d6b97c62d9e0d22",
+    "url": "/static/media/portfoliopic.7be1ac20.JPG"
+  },
+  {
+    "revision": "ed613f727ed82eff5a56c1fc001d73af",
+    "url": "/static/media/pure2.ed613f72.png"
+  },
+  {
+    "revision": "c7dba0cb6b4b8d379b2ab0b97fb69760",
+    "url": "/static/media/quizPic.c7dba0cb.png"
+  },
+  {
+    "revision": "073d8deac8d9a0bd2b9b4b265941c8de",
+    "url": "/static/media/wavel.073d8dea.png"
+  }
+]);
